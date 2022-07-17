bdeb2507c719ac039585da3e32090cd0dac7855a
Ds_mohamed <smdioubate87@yahoo.com>
2022-07-17 11:21:20 +0200

    initial commit

---
+++ src/main/java/mediscreen_Ui/MediscreenUiApplication.java
@@ -0,0 +1,13 @@
+package mediscreen_Ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediscreenUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediscreenUiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mediscreen_Ui/MediscreenUiApplicationTests.java
@@ -0,0 +1,13 @@
+package mediscreen_Ui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediscreenUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
